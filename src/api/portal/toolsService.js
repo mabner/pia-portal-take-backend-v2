fa64b84ad972468c231fa17d0d6256993c6a120b
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-18 00:53:05 -0300

    Resetting the project, node-restful didn't work

--- src/api/portal/toolsService.js
+++
@@ -1,8 +0,0 @@
-const Tools = require( './tools' );
-
-Tools.methods( [ 'get', 'post', 'put', 'delete' ] );
-
-// Enforces validation on updates
-Tools.updateOptions( { new: true, runValidators: true } );
-
-module.exports = Tools;
--- src/loader.js
+++
@@ -1,5 +0,0 @@
-const server = require( './config/server' );
-require( './config/database' );
-
-// Execute the function set in routes.js and pass server as parameter
-require( './config/routes' )( server );
