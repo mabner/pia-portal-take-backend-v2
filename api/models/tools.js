d01a4b0689da316f754f49a17e189451165e6726
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-18 01:17:50 -0300

    tools schema

---
+++ api/models/tools.js
@@ -0,0 +1,24 @@
+const mongoose = require( 'mongoose' );
+
+const toolsSchema = new mongoose.Schema( {
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  usage: {
+    type: String,
+  },
+  tURL: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+} );
+
+module.exports = mongoose.model( 'tools', toolsSchema );
