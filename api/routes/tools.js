97dc611f666173f321aea3938e0f8a3681efb0f2
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-18 02:20:47 -0300

    Patch test

--- api/routes/tools.js
+++ api/routes/tools.js
@@ -1,36 +1,42 @@
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
-const tools = require( '../models/tools' );
+const tools = require('../models/tools');
 
-router.get( '/', async ( req, res ) =>
-{
-  const data = await tools.find();
-  res.send( data );
-} );
+router.get('/', async (req, res) => {
+	const data = await tools.find();
+	res.send(data);
+});
 
-router.get( '/:id', async ( req, res ) =>
-{
-  const data = await tools.findOne( { _id: req.params.id } );
-  res.send( data );
-} );
+router.get('/:id', async (req, res) => {
+	const data = await tools.findOne({
+		_id: req.params.id,
+	});
+	res.send(data);
+});
 
-router.post( '/', async ( req, res ) =>
-{
-  const newTool = new tools( req.body );
-  newTool.save();
-  res.send( 'Cadastro realizado com sucesso.' );
-} );
+router.post('/', async (req, res) => {
+	const newTool = new tools(req.body);
+	newTool.save();
+	res.send('Cadastro realizado com sucesso.');
+});
 
-router.put( '/:id', async ( req, res ) =>
-{
-  await tools.updateOne( { _id: req.params.id } );
-  res.send( 'Cadastro alterado com' );
-} );
+// router.put( '/:id', async ( req, res ) =>
+// {
+//   await tools.updateOne( { _id: req.params.id } );
+//   res.send( 'Cadastro alterado com' );
+// } );
 
-router.delete( '/:id', async ( req, res ) =>
-{
-  await tools.deleteOne( { _id: req.params.id } );
-  res.send( 'Cadastro excluido com sucesso.' );
-} );
+router.patch('/:id', async (req, res) => {
+	const editTool = new tools(req.body);
+	editTool.save();
+	res.send('Cadastro alterado como teste.');
+});
+
+router.delete('/:id', async (req, res) => {
+	await tools.deleteOne({
+		_id: req.params.id,
+	});
+	res.send('Cadastro excluido com sucesso.');
+});
 
 module.exports = router;
