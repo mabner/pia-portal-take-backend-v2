26b4d7ed96d2f27257923600930c36f1b1eccb7d
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-18 03:57:01 -0300

    Put working and route improvements

--- api/config/database.js
+++ api/config/database.js
@@ -3,9 +3,10 @@ const mongoose = require( 'mongoose' );
 const url = process.env.MONGO_ATLAS;
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 };
 
 mongoose.connect( url, options ).then( function ()
--- api/routes/tools.js
+++ api/routes/tools.js
@@ -2,35 +2,77 @@ const express = require('express');
 const router = express.Router();
 const tools = require('../models/tools');
 
+// Get all tools
 router.get('/', async (req, res) => {
-	const data = await tools.find();
-	res.send(data);
+	await tools
+		.find()
+		.then((tools) => {
+			res.status(200).json(tools);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
+// Get tool by ID
 router.get('/:id', async (req, res) => {
-	const data = await tools.findOne({
-		_id: req.params.id,
-	});
-	res.send(data);
+	await tools
+		.findOne({
+			_id: req.params.id,
+		})
+		.then((tool) => {
+			res.status(200).json(tool);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
+// Post a new tool
 router.post('/', async (req, res) => {
 	const newTool = new tools(req.body);
-	newTool.save();
-	res.send('Cadastro realizado com sucesso.');
+	newTool
+		.save()
+		.then((tool) => {
+			res.status(201).json(tool);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
-router.patch('/:id', async (req, res) => {
-	const updateTool = req.body;
-	tools.findOneAndUpdate({ _id: req.params.id }, updateTool, { new: true });
-	res.send('Cadastro atualizado com sucesso.');
+// Update an existing tool
+router.put('/:id', async (req, res) => {
+	await tools
+		.findOneAndUpdate(
+			{
+				_id: req.params.id,
+			},
+			req.body,
+			{
+				new: true,
+			}
+		)
+		.then((tool) => {
+			res.status(200).json(tool);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
+// Delete tool by ID
 router.delete('/:id', async (req, res) => {
-	await tools.deleteOne({
-		_id: req.params.id,
-	});
-	res.send('Cadastro excluido com sucesso.');
+	await tools
+		.findOneAndDelete({
+			_id: req.params.id,
+		})
+		.then((tool) => {
+			res.status(200).json(tool);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
 module.exports = router;
