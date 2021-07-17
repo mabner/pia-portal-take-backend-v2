01067956d7704851f50d5f91d1facaeb637a47a8
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-17 02:18:35 -0300

    cors test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+require('./config/database');
+require('./config/github.strategy');
 
 const express = require('express');
 const PORT = process.env.PORT;
@@ -7,6 +9,7 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const allowCors = require('./config/cors');
 const logger = require('morgan');
+const cors = require('cors');
 
 // Declaring the routes
 const indexRouter = require('./api/routes/index');
@@ -15,15 +18,14 @@ const logoutRouter = require('./api/routes/logout');
 const toolsRouter = require('./api/routes/tools');
 
 const server = express();
-
-require('./config/database');
-require('./config/github.strategy');
 const bodyParser = require('body-parser');
+
 
 server.use(logger('dev'));
 server.use(express.json());
 server.use(allowCors);
 server.use(cookieParser());
+server.use(cors());
 
 // Passport
 server.use(session({secret: 'rainbow bbt', resave: false, saveUninitialized: false}));
@@ -33,8 +35,8 @@ server.use(passport.session());
 
 // Using the routes
 server.use('/', indexRouter);
-server.use('/login', loginRouter);
-server.use('/logout', logoutRouter);
+server.use('/api/v1/login', loginRouter);
+server.use('/api/v1/logout', logoutRouter);
 server.use('/api/v1/tools', toolsRouter);
 
 // Server
