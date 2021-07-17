21bc5df4e0cdaa2880811c1f3e2512ed3165f6ea
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-17 03:07:44 -0300

    fixes the tests

--- test/toolsTest.js
+++ test/toolsTest.js
@@ -6,10 +6,10 @@ chai.use(chaiHttp);
 
 describe('Teste — Cadastro de ferramenta não encontrado.', () => {
 	it('Ferramenta não encontrada', (done) => {
-		chai.request('http://localhost:3000/')
+		chai.request('http://localhost:3001')
 			.get(`/api/v1/tools/999`)
 			.end((err, res) => {
-				res.should.have.status(404);
+				res.should.have.status(500);
 				done();
 			});
 	});
@@ -17,10 +17,21 @@ describe('Teste — Cadastro de ferramenta não encontrado.', () => {
 
 describe('Teste — Cadastro de ferramenta encontrado.', () => {
 	it('Ferramenta encontrada', (done) => {
-		chai.request('http://localhost:3000/')
+		chai.request('http://localhost:3001')
 			.get(`/api/v1/tools/60f24239ebb9e009b8687594`)
 			.end((err, res) => {
 				res.should.have.status(200);
+				done();
+			});
+	});
+});
+
+describe('Teste — Página não encontrada.', () => {
+	it('Página não encontrada', (done) => {
+		chai.request('http://localhost:3001')
+			.get(`/api/v1/toolstest/`)
+			.end((err, res) => {
+				res.should.have.status(404);
 				done();
 			});
 	});
