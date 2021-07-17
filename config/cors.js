000ee17644f50a8c6e976310e77da419ccfa393c
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-17 18:40:23 -0300

    cors test v2

--- config/cors.js
+++ config/cors.js
@@ -2,11 +2,11 @@ module.exports = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Methods',
-		'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
 	);
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept',
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 	next();
 };
